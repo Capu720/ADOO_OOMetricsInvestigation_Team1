edb99a840e0e82c286b244bb1ffc9558b1cbea9b
Jesús Morales <59420444+Capu720@users.noreply.github.com>
2021-03-30 16:26:48 -0600

    Add files via upload

---
+++ ADOO-Ejercicio-2/Persona.java
@@ -0,0 +1,52 @@
+public class Persona
+{
+    public class Tio extends Persona
+    {
+        int funcion1 ( )
+        {
+            if (1 == 1);
+            
+            if (2 == 2);
+
+            if (3 == 3);
+
+            if (3 == 3);
+            
+            if (4 == 4);
+
+            if (5 == 5);
+
+            if (5 == 5);
+
+            return 0;
+        }
+
+        void duerme ()
+        {
+            return;
+        }
+    }
+
+    public class Estudiante extends Persona
+    {
+        void estudiar ( )
+        {
+            return;
+        }
+    }
+
+    public class Carlos extends Tio
+    {
+        
+    }
+
+    public class Sofia extends Estudiante
+    {
+
+    }
+
+    public class Mesme extends Estudiante
+    {
+
+    }
+}
